24e28a248ec6982a168c27023e65b213f07a75e3
Libin Abraham <libinabraham1993@gmail.com>
2022-04-02 22:50:19 +0530

    bcrypt password

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 
+const bcrypt = require("bcryptjs/dist/bcrypt");
 const User = require('../models/User');
+const bcrypt1 = require('bcryptjs');
 
 const {registerValidation} = require('../validation');
 
@@ -12,10 +14,19 @@ router.post('/register', async (req,res) => {
     const {error} = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
+    // Checking the user is exist
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exist');
+
+    // Hash password
+    const salt = await bcrypt1.genSalt(10);
+    const hashedPassword = await bcrypt1.hash(req.body.password, salt);
+
+    // Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
     });
 
     try{
